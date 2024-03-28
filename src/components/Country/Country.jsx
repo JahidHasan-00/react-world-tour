ada8edc746ee17285b4e6c70612cf285a58563fe
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-03-28 11:29:02 +0600

    handleVisited functionality added in button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,26 @@
 
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     console.log(country);
-    const {name, flags, population, area} = country;
+    const {name, flags, population, area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
     return (
-        <div className='country'>
-            <h4>Name: {name?.common} </h4>
-            <img src={flags?.png} alt="" />
-            <p>Population: {population} </p>
-            <p>Area: {area} </p>
-        </div>
+        
+            <div className='country'>
+                <h4>Name: {name?.common} </h4>
+                <img src={flags?.png} alt="" />
+                <p>Population: {population} </p>
+                <p>Area: {area} </p>
+                <p><small>Code: {cca3} </small> </p>
+                <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+                <span>{ visited ? 'I have to visited this country' : 'I want to visit'}</span>
+            </div>
     );
 };
 
